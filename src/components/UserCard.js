17e16ad5c1f1380326862a194672fe424614a2ca
La Baik <labaik237@gmail.com>
2017-02-22 23:41:43 -0800

    add layout to user cards on explore view

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,11 +1,45 @@
 import React, { PropTypes } from 'react'
-import { Card, CardItem, Left, Thumbnail, Body, Text } from 'native-base'
+import { Image } from 'react-native'
+import { Card, CardItem, Left, Right, Thumbnail, Body, Text, Button, Icon } from 'native-base'
 
 function UserCard({ user }) {
   return (
     <Card>
 
       <CardItem>
+        <Left>
+          <Thumbnail source={require('../../images/pepperoni.png')} />
+          <Body>
+            <Text>Student Name</Text>
+            <Text note>Pepperdine Univeristy</Text>
+          </Body>
+        </Left>
+      </CardItem>
+
+      <CardItem cardBody>
+
+      </CardItem>
+
+      <CardItem content>
+        <Text>categories, categories, categories</Text>
+      </CardItem>
+
+      <CardItem>
+        <Left>
+          <Button transparent>
+            <Icon active name="thumbs-up" />
+            <Text>12 Likes</Text>
+          </Button>
+        </Left>
+        <Body>
+          <Button transparent>
+            <Icon active name="chatbubbles" />
+            <Text>4 Reviews</Text>
+          </Button>
+        </Body>
+        <Right>
+          <Text>11h ago</Text>
+        </Right>
       </CardItem>
 
     </Card>
