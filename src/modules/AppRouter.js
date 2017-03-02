002451bffd41fffa85d7fcb27169ac087532721b
La Baik <labaik237@gmail.com>
2017-03-01 17:08:08 -0800

    remove ref to unused counter state

--- src/modules/AppRouter.js
+++ src/modules/AppRouter.js
@@ -1,6 +1,5 @@
 /*eslint-disable react/prop-types*/
 import React from 'react'
-import CounterViewContainer from './counter/CounterViewContainer'
 import ColorViewContainer from './colors/ColorViewContainer'
 import ScheduleViewContainer from './schedule/ScheduleViewContainer'
 import StudentsViewContainer from './students/StudentsViewContainer'
